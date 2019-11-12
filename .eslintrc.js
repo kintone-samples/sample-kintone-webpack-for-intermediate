9c92c5b214cac64c4690eaeb231ea0599289ad86
Kazuki Murahama <kazuki.murahama@gmail.com>
2019-11-12 17:19:02 +0900

    Added lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  // --------------------------------
+  // 呼び出したいルール（パッケージ）
+  // --------------------------------
+  // ES5 & kintone の場合
+  // extends: "@cybozu/eslint-config/presets/kintone-customize-es5",
+
+  // ES6以上 & kintone の場合
+  extends: ["@cybozu", "@cybozu/eslint-config/globals/kintone"],
+
+  // node & kintone の場合
+  // extends: ["@cybozu/eslint-config/presets/node", "@cybozu/eslint-config/globals/kintone"],
+
+  // --------------------------------
+  // グローバル変数の定義
+  // --------------------------------
+  globals: {
+    garoon: "readonly"
+  },
+
+  // --------------------------------
+  //  ルール
+  // --------------------------------
+  rules: {
+    quotes: ["error", "single"]
+  }
+};
